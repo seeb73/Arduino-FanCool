55fb654f08c52c0d5250ed837717a3863d3ecf21
Sebastian <seeb@seeb.net.pl>
2019-06-28 17:28:49 +0200

    Update Sterowanie.ino

--- Sterowanie.ino
+++ Sterowanie.ino
@@ -14,7 +14,6 @@
 //#define DHT11_TYPE DHT22   // DHT 22  (AM2302), AM2321
 DHT dht(DHT11_PIN, DHT11_TYPE);
 
-
 #define ONE_WIRE_BUS 14
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -28,16 +27,14 @@ decode_results results;
 */
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-
 // przekazniki
-int przekaznik1 = 15;
-bool zaCieplo = false;
+int switch1 = 15;
+bool toHot = false; // boolean not work for me!
 int tohot = 24;
 int cool = 20;
 int laststate = 0;
 int fanOnOFF = 0;
-char st = char(223);
-char st1 = (char)223;
+
 void setup() {
   // start serial port
   Serial.begin(9600);
@@ -64,7 +61,7 @@ void setup() {
   lcd.begin(16, 2);  // Inicjalizacja LCD 2x16
   
   // przekazniki setup
-  pinMode(przekaznik1, OUTPUT);
+  pinMode(switch1, OUTPUT);
 }
 
 void loop() {
@@ -77,32 +74,33 @@ void loop() {
 void printTemperature(DeviceAddress deviceAddress)
 {
   float tempC = sensors.getTempC(deviceAddress);
-  float wilgotnosc = dht.readHumidity();
-  float t = dht.readTemperature();
-  char ll = char(223);
+  float humidity = dht.readHumidity();
+  float temp = dht.readTemperature();
   String Celcius = "C";
   String Procent = "%";
-  
+
+  // debug 
   Serial.print("Temp C: ");
   Serial.print(tempC);
   Serial.print(" Temp F: ");
   Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+  Serial.print(humidity);
+  Serial.println(temp);
 
+  // LCD print
   printLcd(0,0,tempC,Celcius,true);
-  printLcd(10,1,t,Procent,false); 
-  printLcd(0,1,t,Celcius,true); 
+  printLcd(10,1,humidity,Procent,false);
+  printLcd(0,1,temp,Celcius,true); 
 }
 
+// boolean not work. Use 0 or 1
 void printLcd(int start, int line,float data, String mySymbol, bool character)
 {
-  char ll = char(223);
-  
   lcd.setCursor(start, line);
   lcd.print(data);
   if (character == 1 ){ 
-      lcd.print(ll);
+      lcd.print(char(223));
     }
-
   lcd.print(mySymbol);
 }
 
@@ -120,32 +118,28 @@ void setState(float temperature)
 {
   if (temperature >= tohot )
   {
-    digitalWrite(przekaznik1, HIGH);
+    digitalWrite(switch1, HIGH);
     fanOnOFF = 1;
     laststate = 1;
   }
   else if (temperature < tohot && laststate == 1 && temperature > cool )
   {
-   Serial.println("Chłodzimy dalej"); 
-   digitalWrite(przekaznik1, HIGH);
+   Serial.println("Continue cooling / Chłodzimy dalej"); 
+   digitalWrite(switch1, HIGH);
    laststate = 1;
    fanOnOFF = 1;
   } else 
   {
-    digitalWrite(przekaznik1, LOW);
+    digitalWrite(switch1, LOW);
     laststate = 0;
     fanOnOFF = 0;
     }
 }
 
+// boolean not work on arduino
 int lastState()
 {
-  if (laststate > 1)
-  {
-   return 0; 
-  }
-  
-  if (laststate == 0)
+  if (laststate == 0 || laststate > 1 )
   {
     return 0;
    } else if (laststate == 1 )
@@ -156,13 +150,12 @@ int lastState()
 
 void printFanStatus()
 {
-  if (fanOnOFF == 1)
-  {
-    lcd.setCursor(10, 0);
-    lcd.print("FAN:On");
-  } else 
-  {
-    lcd.setCursor(9, 0);
-    lcd.print("FAN:Off");
-  }
+  lcd.setCursor(9, 0);
+    if (fanOnOFF == 1)
+    {
+      lcd.print(" FAN:On");
+    } else 
+    {
+      lcd.print("FAN:Off");
+    }
 }
